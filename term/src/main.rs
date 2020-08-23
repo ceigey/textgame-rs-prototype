bf7c654c655bda28e2b76cf6e42626cf867b16aa
Christian Meredith <ceigey@users.noreply.github.com>
2020-08-23 05:04:13 +0000

    Learning about lifetime overlaps

--- term/src/main.rs
+++ term/src/main.rs
@@ -25,8 +25,11 @@ impl World {
     fn new() -> World {
         World {
             entities: HashMap::new(),
-            portals: Vec::new(),
-            spaces: HashMap::new()
+            spaces: HashMap::new(),
+            // Portals have no hashmap
+            // access is almost always
+            // done by searching :(
+            portals: Vec::new()
         }
     }
 
@@ -68,17 +71,77 @@ impl World {
         entity.space = space.name.clone();
         Ok(self)
     }
+
+    // TODO: use some metaprogramming!
+    // TODO: I want filters, no?!
+    fn find_mut_portal_to(&mut self, to: &str) -> Option<&mut Portal> {
+        self.portals
+            .iter_mut()
+            .find(|p| p.to == to)
+    }
+
+    fn find_mut_portal_from(&mut self, from: &str) -> Option<&mut Portal> {
+        self.portals
+            .iter_mut()
+            .find(|p| p.from == from)
+    }
+
+    fn find_portal_to(&self, to: &str) -> Option<&Portal> {
+        self.portals
+            .iter()
+            .find(|p| p.to == to)
+    }
+
+    fn find_portal_from(&self, from: &str) -> Option<&Portal> {
+        self.portals
+            .iter()
+            .find(|p| p.from == from)
+    }
 }
 
 fn game_loop() {
     let mut world = World::new();
     world
-        .add_entity("player")
-        .add_space("room1");
+        .add_entity("P")
+        .add_space("R1")
+        .add_space("R2")
+        .add_space("R3")
+        .add_portal("R1", "R2")
+        .add_portal("R1", "R3")
+        .add_portal("R2", "R3")
+        .add_portal("R2", "R1")
+        .add_portal("R3", "R1");
     
-    world.teleport_entity_to_space("player", "room1").unwrap();
-    
-    println!("Player space is: {}", world.entities.get("player").map(|e| e.space.clone()).unwrap_or(String::from("Nowhere...")));
+    world
+        .teleport_entity_to_space("P", "R1")
+        .unwrap();
+
+    let mut def_p = Portal {
+        to: String::from("VOID"),
+        from: String::from("VOID")
+    };
+
+    let r3_mp = world
+        .find_mut_portal_from("R3")
+        .unwrap_or(&mut def_p);
+    //We have to copy data before
+    //passing world around again
+    let r3_to = r3_mp.to.clone();
+    let r3_from = r3_mp.from.clone();
+
+    let player_space = world.entities
+        .get_mut("player")
+        .map(|e| e.space.clone())
+        .unwrap_or(String::from("Nowhere..."));
+    println!(
+        "Player space is: {}",
+        player_space
+    );
+
+    println!(
+        "R3 first portal was: {}<-{}",
+        r3_to, r3_from
+    );
 }
 
 
